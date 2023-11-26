50b627b9ec7897246d3d596707e9c1cb5d0c46bf
Clarfl <101613850+Clarfl@users.noreply.github.com>
2023-11-26 12:15:38 -0300

    banco
    inserindo o arquivo do banco

---
+++ banco.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 26/11/2023 às 16:14
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `banco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `atendimento`
+--
+
+CREATE TABLE `atendimento` (
+  `idAtendimento` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `idServico` int(11) DEFAULT NULL,
+  `idFilial` int(11) DEFAULT NULL,
+  `dataAtendimento` date NOT NULL,
+  `horarioPreferencial` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `filial`
+--
+
+CREATE TABLE `filial` (
+  `idFilial` int(11) NOT NULL,
+  `descricao_F` varchar(50) NOT NULL,
+  `localização` varchar(50) NOT NULL,
+  `responsável` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `filial`
+--
+
+INSERT INTO `filial` (`idFilial`, `descricao_F`, `localização`, `responsável`) VALUES
+(1, 'opa', 'ilheus', 'eu'),
+(2, 'legal', 'brasil', 'minha mae'),
+(3, 'opa', 'ilheus', 'eu');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `servico`
+--
+
+CREATE TABLE `servico` (
+  `idServico` int(11) NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  `preco` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `servico`
+--
+
+INSERT INTO `servico` (`idServico`, `descricao`, `preco`) VALUES
+(1, 'cabelo', 20),
+(3, 'barba', 10),
+(4, 'bigode', 5),
+(5, 'cabelo', 20),
+(6, 'ddawda', 323);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `atendimento`
+--
+ALTER TABLE `atendimento`
+  ADD PRIMARY KEY (`idAtendimento`),
+  ADD KEY `idServico` (`idServico`),
+  ADD KEY `idFilial` (`idFilial`);
+
+--
+-- Índices de tabela `filial`
+--
+ALTER TABLE `filial`
+  ADD PRIMARY KEY (`idFilial`);
+
+--
+-- Índices de tabela `servico`
+--
+ALTER TABLE `servico`
+  ADD PRIMARY KEY (`idServico`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `atendimento`
+--
+ALTER TABLE `atendimento`
+  MODIFY `idAtendimento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `filial`
+--
+ALTER TABLE `filial`
+  MODIFY `idFilial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `idServico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `atendimento`
+--
+ALTER TABLE `atendimento`
+  ADD CONSTRAINT `atendimento_ibfk_1` FOREIGN KEY (`idServico`) REFERENCES `servico` (`idServico`),
+  ADD CONSTRAINT `atendimento_ibfk_2` FOREIGN KEY (`idServico`) REFERENCES `servico` (`idServico`),
+  ADD CONSTRAINT `atendimento_ibfk_3` FOREIGN KEY (`idServico`) REFERENCES `servico` (`idServico`),
+  ADD CONSTRAINT `atendimento_ibfk_4` FOREIGN KEY (`idFilial`) REFERENCES `filial` (`idFilial`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
